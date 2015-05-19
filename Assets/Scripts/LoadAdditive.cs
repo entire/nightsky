0a93d37ae6ac37fcf12227300468cc14773f17ec
Kosuke Hata <hata.kosuke@gmail.com>
2015-05-18 23:34:50 -0700

    more stuff was added

---
+++ Assets/Scripts/LoadAdditive.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadAdditive : MonoBehaviour {
+
+	// Use this for initialization
+	public void LoadAddOnClick(int level) 
+	{
+		Application.LoadLevelAdditive(level);
+	}
+}
